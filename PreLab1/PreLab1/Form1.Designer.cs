17c897c0abfc291a186d8764f32d3173f1a87a7e
Sevval_Kolaca <sevval.kolaca.00@gmail.com>
2022-03-24 14:15:46 +0300

    Açılış ekranını oluşturdum. Butonlar inaktif.

---
+++ PreLab1/PreLab1/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace PreLab1
+{
+    partial class LoginScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnLogin = new System.Windows.Forms.Button();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnLogin
+            // 
+            this.BtnLogin.Location = new System.Drawing.Point(199, 242);
+            this.BtnLogin.Name = "BtnLogin";
+            this.BtnLogin.Size = new System.Drawing.Size(157, 40);
+            this.BtnLogin.TabIndex = 3;
+            this.BtnLogin.Text = "Log In";
+            this.BtnLogin.UseVisualStyleBackColor = true;
+            this.BtnLogin.MouseCaptureChanged += new System.EventHandler(this.BtnLogin_MouseCaptureChanged);
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.AcceptsTab = true;
+            this.txtUserName.Location = new System.Drawing.Point(199, 135);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(157, 22);
+            this.txtUserName.TabIndex = 1;
+            this.txtUserName.UseWaitCursor = true;
+            this.txtUserName.CursorChanged += new System.EventHandler(this.txtUserName_CursorChanged);
+            this.txtUserName.TextChanged += new System.EventHandler(this.txtUserName_TextChanged);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(199, 184);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(157, 22);
+            this.txtPassword.TabIndex = 2;
+            this.txtPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(56, 140);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(126, 17);
+            this.lblName.TabIndex = 3;
+            this.lblName.Text = "User/Admin Name:";
+            this.lblName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(109, 189);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(73, 17);
+            this.lblPassword.TabIndex = 4;
+            this.lblPassword.Text = "Password:";
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.Location = new System.Drawing.Point(419, 388);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(122, 40);
+            this.btnSettings.TabIndex = 4;
+            this.btnSettings.Text = "Settings";
+            this.btnSettings.UseVisualStyleBackColor = true;
+            // 
+            // LoginScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 450);
+            this.Controls.Add(this.btnSettings);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.BtnLogin);
+            this.Name = "LoginScreen";
+            this.Text = "Log-in Screen";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnLogin;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Button btnSettings;
+    }
+}
+
--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # OOP_2_Lab
 Project of OOP 2 Lab
-
-Deneme 1  -Şevval Kolaca
